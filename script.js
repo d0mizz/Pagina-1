68ec0a61a3c990700a9d29a6c201dd6a7b47a40d
Geglash08 <132537069+Geglash08@users.noreply.github.com>
2023-05-05 19:40:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -51,3 +51,56 @@ function leerDatosElemento(elemento) {
     insertarCarrito(infoElemento)
 }
 
+function insertarCarrito(elemento) {
+
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    
+        <td>
+            <img src="${elemento.imagen}" width=100  />     
+        </td>
+
+        <td>
+
+            ${elemento.titulo}
+
+        </td>
+
+        <td>
+            ${elemento.precio}
+        </td>
+
+        <td>
+
+            <a = herf="#" class="borrar" data-id="${elemento.id}" >X</a>
+
+        </td>
+    
+    
+    `;
+    
+    lista.appendChild(row);
+
+}
+
+function eliminarElemento(e) {
+
+    e.preventDefault();
+    let elemento,
+        elementoId;
+
+    if(e.target.classList.contains('borrar')) {
+        e.target.parentElement.parentElement.remove();
+        elemento = e.target.parentElement.parentElement;
+        elementoId = elemento.querySelector('a').getAttribute('data-id');
+    }
+
+}
+
+function vaciarCarrito() {
+    while(lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+    return false;
+
+}
